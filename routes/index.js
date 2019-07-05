d31a423e84c819eb406f51f2f1525ce504c126b8
Sergio Festa <sfestatomari@gmail.com>
2019-07-05 12:13:30 -0300

    done

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models/book');
+
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.get('/books', (req, res) => {
+  Book.find()
+    .then((books) => {
+      res.render('books', { books });
+    })
+    .catch(err => console.log(err));
+});
+
+router.get('/book/:bookID', (req, res) => {
+  const book = req.params.bookID;
+  Book.findById(book)
+    .then((book) => {
+      res.render('book-details', book);
+    })
+    .catch(err => console.log(err));
+});
+
+module.exports = router;
